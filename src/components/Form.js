38639ff620cab43b5f4518f016484a4b350b1133
linilinilyn~ <linilinilyn@gmail.com>
2023-06-14 11:39:31 +0900

    feat : Tailwind 적용

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,9 +25,9 @@ const Form = ({ todoData, setTodoData }) => {
   };
   return (
     <div>
-      <form style={{ display: "flex", gap: "0 10px" }} onSubmit={handleSubmit}>
-        <input type="text" name="value" style={{ flex: "10", padding: "5px" }} placeholder="할일을 입력해 주세요" value={value} onChange={handleChange} />
-        <input type="submit" style={{ flex: "1" }} value="입력" />
+      <form className="flex pt-2" style={{ display: "flex", gap: "0 10px" }} onSubmit={handleSubmit}>
+        <input type="text" name="value" style={{ flex: "10", padding: "5px" }} placeholder="할일을 입력해 주세요" value={value} onChange={handleChange} className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow" />
+        <input type="submit" style={{ flex: "1" }} value="입력" className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400" />
       </form>
     </div>
   );
